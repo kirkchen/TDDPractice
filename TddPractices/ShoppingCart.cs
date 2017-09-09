eba5b7650349b2375e7ae5af1c60cffd2ef0e9da
Kirk Chen <rwk0119@yahoo.com.tw>
2017-09-09 19:33:16 +0800

    重構: 提取 totalPrice, 新增 discountRate 作為折扣比例

--- TddPractices/ShoppingCart.cs
+++ TddPractices/ShoppingCart.cs
@@ -11,16 +11,17 @@ namespace TddPractices
         public double Calculate(string level, double price, int qty)
         {
             var totalPrice = price * qty;
+            var discountRate = 1d;
             if(level == "VIP" && totalPrice > 500)
             {
-                return totalPrice * 0.8;
+                discountRate = 0.8;
             }
             else if (totalPrice > 1000 && qty > 3)
             {
-                return totalPrice * 0.85;
+                discountRate = 0.85;
             }
 
-            return totalPrice;
+            return totalPrice * discountRate;
         }
     }
 }
