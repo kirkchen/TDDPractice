0ff533b8ee58b8004b55d9868ef9293cb9f745c8
Kirk Chen <rwk0119@yahoo.com.tw>
2017-09-09 19:25:22 +0800

    實作 VIP 會員, 購買 150 元商品 5 件, 結帳金額為 600 元邏輯

--- TddPractices/ShoppingCart.cs
+++ TddPractices/ShoppingCart.cs
@@ -10,6 +10,12 @@ namespace TddPractices
 
         public double Calculate(string level, double price, int qty)
         {
+            var totalPrice = price * qty;
+            if(totalPrice > 500)
+            {
+                return totalPrice * 0.8;
+            }
+
             return 450;
         }
     }
