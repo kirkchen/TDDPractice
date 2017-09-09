ad0b9afa844b053ee3058030326596e6be6465ca
Kirk Chen <rwk0119@yahoo.com.tw>
2017-09-09 19:29:52 +0800

    新增測試, 一般 會員, 購買 600 元商品 2 件, 結帳金額為 1200 元

--- TddPractices.Tests/ShoppingCartTest.cs
+++ TddPractices.Tests/ShoppingCartTest.cs
@@ -20,6 +20,7 @@ namespace TddPractices.Tests
         [InlineData("VIP", 150, 5, 600)]
         [InlineData("NORMAL", 300, 2, 600)]
         [InlineData("NORMAL", 600, 4, 2040)]
+        [InlineData("NORMAL", 600, 2, 1200)]
         public void Test_ShoppingCart_Calculate(string level, double price, int qty, double expected)
         {
             // Arrange
