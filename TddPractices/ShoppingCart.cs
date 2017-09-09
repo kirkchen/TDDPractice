b2062c1fa9bc8c69ee2cb3daea1717c1825c14b3
Kirk Chen <rwk0119@yahoo.com.tw>
2017-09-09 19:27:07 +0800

    實作會員 level 判斷邏輯

--- TddPractices/ShoppingCart.cs
+++ TddPractices/ShoppingCart.cs
@@ -11,12 +11,12 @@ namespace TddPractices
         public double Calculate(string level, double price, int qty)
         {
             var totalPrice = price * qty;
-            if(totalPrice > 500)
+            if(level == "VIP" && totalPrice > 500)
             {
                 return totalPrice * 0.8;
             }
 
-            return 450;
+            return totalPrice;
         }
     }
 }
